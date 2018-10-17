cb16338f406ab833f97ac057ba30b8a5de1c1488
PhilipBadilla <philip.c.p.badilla.hybrid@gmail.com>
2018-10-17 20:58:23 +0800

    INIT #comment Initial Commit

---
+++ README.md
@@ -0,0 +1,100 @@
+# safe-defer.js
+`safe-defer.js` is a simple, SEO friendly, pure Javascript library for deferring image loading, class appending, and applying styles. Its aim is to be a middle ground for achieving improved page load performance and SEO compliance.
+### SEO friendly
+Although in theory, crawlers execute scripts so they will see the replaced images. This approach allows SEO crawlers to locate and index your images without the need for javascript to run.
+
+before defer process
+```html
+<img src="my-image.png" data-safe-defer-src/> 
+```
+during defer process (starts when DOM is ready)
+```html
+<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" 
+    data-safe-deferred-src="my-image.png"/> 
+```
+after defer process
+```html
+<img src="my-image.png" /> 
+```
+### Defer CSS Classes
+Although CSS `class` would rarely include definition for images and icons they can also be deferred(not safely) using this library.
+
+before defer process
+```html
+<div class="my-custom-icon" data-safe-defer-class="left-arrow-icon"/> 
+```
+after defer process
+```html
+<img class="my-custom-icon left-arrow-icon" /> 
+```
+### Safe-Defer Inline Styles
+In-case you need to use inline style with your `background-image` you can also safely defer them.
+
+before defer process
+```html
+<div style="background-image: url('/images/my-wallpaper.png");" data-safe-defer-style/> 
+```
+during defer process (starts when DOM is ready)
+```html
+<div style="" data-safe-defer-style="background-image: url('/images/my-wallpaper.png");" /> 
+```
+after defer process
+```html
+<div style="background-image: url('/images/my-wallpaper.png");"/> 
+```
+### IDE friendly
+Since we don't use image placeholders by default, you can still see your images while designing your HTML pages.
+
+# Usage
+Add the library just before the HTML body closing tag
+  ```html
+  <html>
+    <head>
+        // meta stuff
+    </head>
+    <body>
+        // DOM stuff
+        // More DOM stuff
+        <script type="text/javascript" src="safe-defer.min.js"></script>
+        // other cool scripts & stylesheet references
+    </body>
+  </html>
+  ```
+### Defer a `src`
+Add the `data-safe-defer-src` attribute to an `img` element to defer the source.
+```html
+<img src="my-image.png" data-safe-defer-src/> 
+```
+Disclaimer: `data-safe-defer-src` would work on any HTML element that has `src` attribute however it was only tested on image tags.
+
+### Defer a `class` 
+Place the class to be deferred in the `data-safe-defer-class` attribute.
+```html
+<div class="my-custom-icon" data-safe-defer-class="left-arrow-icon"/> 
+```
+### Defer a `style` 
+Add the `data-safe-defer-style` attribute to any HTML element where you want the style to be deferred.
+```html
+<div style="background-image: url('/images/my-wallpaper.png");" data-safe-defer-style/> 
+```
+WARNING: This will defer everything inside the `style` attribute of the element.
+
+## Browser Support
+
+![Chrome][chrome-image] | ![Firefox][firefox-image] | ![IE][ie-image] | ![Opera][opera-image] | ![Safari][safari-image]
+--- | --- | --- | --- | --- |
+Latest ✔ | Latest ✔ | 10+ ✔ | Latest ✔ | 6.1+ ✔ |
+# License
+MIT
+
+### Donations
+If this repo has helped you and you're feeling generous, please feel free to tip!
+##### Paypal - `paypal.me/philipbadilla`
+##### BTC - `1CDBQodi5FkvDevi4M7UtkXxtzfseCUJqW`
+##### LTC - `Lf5o6FCKzuiqAtkz7tTqr2b6NKA4hUgYS5`
+
+[chrome-image]: https://raw.github.com/alrra/browser-logos/master/src/chrome/chrome_48x48.png
+[firefox-image]: https://raw.github.com/alrra/browser-logos/master/src/firefox/firefox_48x48.png
+[ie-image]: https://raw.github.com/alrra/browser-logos/master/src/archive/internet-explorer_9-11/internet-explorer_9-11_48x48.png
+[opera-image]: https://raw.github.com/alrra/browser-logos/master/src/opera/opera_48x48.png
+[safari-image]: https://raw.github.com/alrra/browser-logos/master/src/safari/safari_48x48.png
---
+++ src/safe-defer.js
@@ -0,0 +1,132 @@
+var safeDefer = {
+  srcDeferAttribute: "data-safe-defer-src",
+  styleDeferAttribute: "data-safe-defer-style",
+  classDeferAttribute: "data-safe-defer-class",
+  srcBackingAttribute: "data-safe-deferred-src",
+  imagePlaceholder: "data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+  debugMode: false,
+
+  deferAll: function() {
+    safeDefer.deferSources();
+    safeDefer.deferStyle();
+  },
+
+  deferSources: function() {
+    // Find all elements that requires src attribute to be deferred
+    var selector =  "*[" + safeDefer.srcDeferAttribute + "]";
+    var elements = document.querySelectorAll(selector);
+
+    // Iterate each element
+    for(var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var originalSource = element.getAttribute("src");
+      if (safeDefer.debugMode) {
+        console.log("Deferring Source: " + originalSource);
+      }
+      // Only defer sources that aren't loaded yet
+      var notFullyLoaded = !element.complete;
+      if (notFullyLoaded) {
+        // Move original src to back-up attribute and use a placeholder on src
+        element.setAttribute(safeDefer.srcBackingAttribute, originalSource);
+        element.setAttribute("src", safeDefer.imagePlaceholder);
+      }
+      // Clean up the DOM, we no longer need this attribute
+      element.removeAttribute(safeDefer.srcDeferAttribute);
+    }
+  },
+
+  deferStyle: function() {
+    // Find all elements that requires style attribute to be deferred
+    var selector =  "*[" + safeDefer.styleDeferAttribute + "]";
+    var elements = document.querySelectorAll(selector);
+
+    // Iterate each element
+    for(var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var orignalStyle = element.getAttribute("style");
+      if (safeDefer.debugMode) {
+        console.log("Deferring Style: " + orignalStyle);
+      }
+      // Move original style to back-up attribute and remove current style 
+      element.setAttribute(safeDefer.styleDeferAttribute, orignalStyle);
+      element.setAttribute("style", "");
+    }
+  },
+
+  loadAllDeferred: function() {
+    safeDefer.loadDeferredClasses();
+    safeDefer.loadDeferredSources();
+    safeDefer.loadDeferredStyles();
+  },
+
+  loadDeferredSources: function() {
+    // Find all elements that has deferred src attribute
+    var selector =  "*[" + safeDefer.srcBackingAttribute + "]";
+    var elements = document.querySelectorAll(selector);
+
+    // Iterate each element
+    for(var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var deferredSource = element.getAttribute(safeDefer.srcBackingAttribute);
+      if (safeDefer.debugMode) {
+        console.log("Loading Source: " + deferredSource);
+      }
+      // Apply the defered source
+      element.setAttribute("src", deferredSource);
+      // Clean up the DOM, we no longer need this attribute
+      element.removeAttribute(safeDefer.srcBackingAttribute);
+    }
+  },
+
+  loadDeferredClasses: function() {
+    // Find all elements that has deferred class attribute
+    var selector =  "*[" + safeDefer.classDeferAttribute + "]";
+    var elements = document.querySelectorAll(selector);
+
+    // Iterate each element
+    for(var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var deferredClass = element.getAttribute(safeDefer.classDeferAttribute);
+      if (safeDefer.debugMode) {
+        console.log("Appending Class: " + deferredClass);
+      }
+      // Append deferred class
+      element.classList.add(deferredClass);
+      // Clean up the DOM, we no longer need this attribute
+      element.removeAttribute(safeDefer.classDeferAttribute);
+    }
+  },
+
+  loadDeferredStyles: function() {
+    // Find all elements that has deferred style attribute
+    var selector =  "*[" + safeDefer.styleDeferAttribute + "]";
+    var elements = document.querySelectorAll(selector);
+
+    // Iterate each element
+    for(var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var deferredStyle = element.getAttribute(safeDefer.styleDeferAttribute);
+      if (safeDefer.debugMode) {
+        console.log("Setting Style: " + deferredStyle);
+      }
+      // Apply the deferred style
+      element.setAttribute("style", deferredStyle);
+      // Clean up the DOM, we no longer need this attribute
+      element.removeAttribute(safeDefer.styleDeferAttribute);
+    }
+  }
+};
+
+(function() {
+  if (safeDefer.debugMode) {
+    console.log("DOM is available!");
+  }
+  safeDefer.deferAll();
+})();
+
+window.addEventListener("load", function () {
+  if (safeDefer.debugMode) {
+    console.log("Page load complete!");
+  }
+  safeDefer.loadAllDeferred();
+}, false);
